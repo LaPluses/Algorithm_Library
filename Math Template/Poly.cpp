0271668745e4cdc9d3ebccc4505dfe8c822c1aac
xiper <xipersup@gmail.com>
2017-07-10 21:42:07 +0800

    Modify FFT(mod 1e9 + 7)

--- Math Template/Poly.cpp
+++ Math Template/Poly.cpp
@@ -23,8 +23,9 @@ namespace Poly{
 
 	const static int maxn = 1 << 18;
 	const double pi = acos( -1 );
+	const int mod = 1e9 + 7;
 
-	Complex w1[maxn + 1] , w2[maxn + 1] , fr[maxn] , A[maxn] , B[maxn];
+	Complex w1[maxn + 1] , w2[maxn + 1] , fr[maxn] , fw[maxn] , A[maxn] , B[maxn] , C[maxn] , D[maxn];
 
 	void Prepare(){
 		for(int i = 1 ; i <= maxn ; ++ i)
@@ -59,20 +60,41 @@ namespace Poly{
 			}
 	}
 
-	void Convex( int * x , int * y , int n ){
-		for(int i = 0 ; i < n ; ++ i)
-			fr[i] = Complex( x[i] , y[i] );
-		DFT( fr , n , 1 );
+	void DFT( Complex * x , int n , Complex * r1 = A , Complex * r2 = B ){
+		DFT( x , n , 1 );
 		for(int i = 0 ; i < n ; ++ i){
 			int j = ( n - i ) & (n - 1);
-			A[i] = ( fr[i] + fr[j].Conj() ) * Complex( 0.5 , 0 );
-			B[i] = ( fr[i] - fr[j].Conj() ) * Complex( 0 , -0.5 );
+			r1[i] = ( x[i] + x[j].Conj() ) * Complex( 0.5 , 0 );
+			r2[i] = ( x[i] - x[j].Conj() ) * Complex( 0 , -0.5 );
 		}
 	}
 
+	void Mul( int * x , int * y , int * z , int n ){
+		for(int i = 0 ; i < n ; ++ i){
+			fr[i] = Complex( x[i] >> 15 , x[i] & 32767 );
+			fw[i] = Complex( y[i] >> 15 , y[i] & 32767 );
+		}
+		DFT( fr , n , A , B );
+		DFT( fw , n , C , D );
+		for(int i = 0 ; i < n ; ++ i){
+			fr[i] = A[i] * C[i];
+			fw[i] = A[i] * D[i] + B[i] * C[i];
+			A[i] = B[i] * D[i];
+		}
+		DFT( fr , n , -1 );
+		DFT( fw , n , -1 );
+		DFT( A , n , -1 );
+		for(int i = 0 ; i < n ; ++ i){
+			int a = (long long)( fr[i].r / n + 0.5 ) % mod;
+			int b = (long long)( fw[i].r / n + 0.5 ) % mod;
+			int c = (long long)( A[i].r / n + 0.5 ) % mod;
+			z[i] = ( ((long long)a << 30LL) + ((long long)b << 15LL) + c ) % mod;
+		}
+	}
 };
 
 int main( int argc , char * argv[] ){
 	Poly::Prepare();
 	return 0;
 }
+
